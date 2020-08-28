539ce993e65ce52e3b75c93ff2ae5ef0e884fb25
nickbabcock <nbabcock19@hotmail.com>
2020-08-28 07:02:14 -0500

    Add benchmark for parsing eu4dates

--- benches/eu4save_bench.rs
+++ benches/eu4save_bench.rs
@@ -1,4 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+use eu4save::Eu4Date;
 
 const METADATA_BIN: &'static [u8] = include_bytes!("../tests/fixtures/meta.bin");
 
@@ -12,5 +13,19 @@ pub fn melt_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, melt_benchmark);
+pub fn eu4_date_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("eu4date-parse");
+    group.bench_function("valid-date", |b| {
+        b.iter(|| Eu4Date::parse_from_str("1444.11.11").unwrap())
+    });
+    group.bench_function("invalid-date", |b| {
+        b.iter(|| Eu4Date::parse_from_str("marketplace").is_none())
+    });
+    group.bench_function("long-invalid-date", |b| {
+        b.iter(|| Eu4Date::parse_from_str("incidents_bur_inheritance.5").is_none())
+    });
+    group.finish();
+}
+
+criterion_group!(benches, melt_benchmark, eu4_date_benchmark);
 criterion_main!(benches);
