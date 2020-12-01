84fd238f53050fe6f9cdd7990ec1260d9feaa876
Nick Babcock <nbabcock19@hotmail.com>
2020-12-01 16:43:43 -0600

    Make deducer generic

--- src/bin/deducer.rs
+++ src/bin/deducer.rs
@@ -1,27 +1,28 @@
-use eu4save::{
-    models::{Country, Eu4Save},
-    Eu4Extractor,
-};
-use std::collections::HashSet;
+use eu4save::Eu4Extractor;
 use std::env;
 use std::io::Cursor;
+use std::{collections::HashSet, fmt::Display};
 
 #[derive(Debug)]
-struct Deduce {
+struct Deduce<N> {
     country: String,
     index: usize,
-    value: f32,
+    value: N,
 }
 
-fn deduce_vec(save: &Eu4Save, f: impl Fn(&Country) -> &[f32]) {
+fn deduce_vec<'a, N>(iter: impl Iterator<Item = (&'a str, &'a [N])>)
+where
+    N: 'a + PartialEq + Default + Display,
+{
     let mut ded = Vec::new();
     let mut found_indices = HashSet::new();
     let mut max_indices = 0;
+    let default_val = N::default();
 
-    for (tag, country) in &save.game.countries {
-        max_indices = std::cmp::max(max_indices, f(country).len());
-        for (i, &value) in f(country).iter().enumerate() {
-            if value != 0.0 && !found_indices.contains(&i) {
+    for (tag, vals) in iter {
+        max_indices = std::cmp::max(max_indices, vals.len());
+        for (i, value) in vals.iter().enumerate() {
+            if value.ne(&default_val) && !found_indices.contains(&i) {
                 found_indices.insert(i);
                 ded.push(Deduce {
                     index: i,
@@ -44,16 +45,31 @@ fn deduce_vec(save: &Eu4Save, f: impl Fn(&Country) -> &[f32]) {
             missing_indices.push(i);
         }
     }
-
-    println!("missing {:?}", missing_indices);
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let data = std::fs::read(&args[1])?;
     let (save, _encoding) = Eu4Extractor::extract_save(Cursor::new(&data[..]))?;
-    deduce_vec(&save, |c| &c.ledger.income);
-    deduce_vec(&save, |c| &c.ledger.expense);
+    deduce_vec(
+        save.game
+            .countries
+            .iter()
+            .map(|(tag, c)| (tag.as_str(), c.ledger.income.as_slice())),
+    );
+    deduce_vec(
+        save.game
+            .countries
+            .iter()
+            .map(|(tag, c)| (tag.as_str(), c.ledger.expense.as_slice())),
+    );
+    deduce_vec(
+        save.game
+            .countries
+            .iter()
+            .filter(|(_tag, c)| c.num_of_cities > 0)
+            .map(|(tag, c)| (tag.as_str(), c.losses.members.as_slice())),
+    );
 
     Ok(())
 }
