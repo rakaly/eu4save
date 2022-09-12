a17365c735e689927e6c77f718350e41d8a6775b
Nick Babcock <nbabcock19@hotmail.com>
2022-09-12 06:16:24 -0500

    Allow ownership transfer of melted output

--- src/melt.rs
+++ src/melt.rs
@@ -198,6 +198,11 @@ pub struct MeltedDocument {
 
 impl MeltedDocument {
     /// The converted plaintext data
+    pub fn into_data(self) -> Vec<u8> {
+        self.data
+    }
+
+    /// The converted plaintext data
     pub fn data(&self) -> &[u8] {
         self.data.as_slice()
     }
