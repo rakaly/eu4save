12abfb027a457c22e94345aebfe4bd26d6d9ae28
Nick Babcock <nbabcock19@hotmail.com>
2025-04-04 07:53:56 -0500

    Add more benchmarks

--- bench/src/bench.rs
+++ bench/src/bench.rs
@@ -1,9 +1,11 @@
 use criterion::{Criterion, Throughput};
+use eu4save::flavor::Eu4Flavor;
+use jomini::JominiDeserialize;
 use serde::{
     de::{self, SeqAccess},
     Deserialize, Deserializer,
 };
-use std::fmt;
+use std::{collections::HashMap, fmt};
 
 fn parse_save(c: &mut Criterion) {
     let mut group = c.benchmark_group("parse_save");
@@ -74,24 +76,57 @@ fn parse_save(c: &mut Criterion) {
 fn deserialize_ledger(c: &mut Criterion) {
     let mut group = c.benchmark_group("deserialize");
 
-    #[derive(Debug, Clone, Deserialize)]
+    #[derive(Debug, Clone, JominiDeserialize)]
     #[expect(dead_code)]
     struct CountryLedger {
-        #[serde(deserialize_with = "deserialize_list")]
+        #[jomini(
+            default = "array_default",
+            deserialize_with = "deserialize_list",
+            token = 0x287f
+        )]
         income: [f32; 19],
-        #[serde(deserialize_with = "deserialize_list")]
+        #[jomini(
+            default = "array_default",
+            deserialize_with = "deserialize_list",
+            token = 0x2880
+        )]
         expense: [f32; 38],
-        #[serde(alias = "lastmonthincome")]
+        #[jomini(alias = "lastmonthincome", token = 0x2879)]
         last_month_income: Option<f32>,
-        #[serde(alias = "lastmonthincometable", deserialize_with = "deserialize_list")]
+        #[jomini(
+            alias = "lastmonthincometable",
+            default = "array_default",
+            deserialize_with = "deserialize_list",
+            token = 0x287b
+        )]
         last_month_income_table: [f32; 19],
-        #[serde(alias = "lastmonthexpensetable", deserialize_with = "deserialize_list")]
+        #[jomini(
+            alias = "lastmonthexpensetable",
+            default = "array_default",
+            deserialize_with = "deserialize_list",
+            token = 0x287c
+        )]
         last_month_expense_table: [f32; 38],
-        #[serde(alias = "totalexpensetable", deserialize_with = "deserialize_list")]
+        #[jomini(
+            alias = "totalexpensetable",
+            default = "array_default",
+            deserialize_with = "deserialize_list",
+            token = 0x34a4
+        )]
         total_expense_table: [f32; 38],
-        #[serde(alias = "lastyearincome", deserialize_with = "deserialize_list")]
+        #[jomini(
+            alias = "lastyearincome",
+            default = "array_default",
+            deserialize_with = "deserialize_list",
+            token = 0x287d
+        )]
         last_year_income: [f32; 19],
-        #[serde(alias = "lastyearexpense", deserialize_with = "deserialize_list")]
+        #[jomini(
+            alias = "lastyearexpense",
+            default = "array_default",
+            deserialize_with = "deserialize_list",
+            token = 0x287e
+        )]
         last_year_expense: [f32; 38],
     }
 
@@ -123,11 +158,59 @@ fn deserialize_ledger(c: &mut Criterion) {
 			last_months_recurring_expenses=409.644
 "#;
 
+    let data2 = r#"
+			incom1={
+				160.500 539.364 817.406 52.862 20.882 1.665 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 5.652
+			}
+			expens1={
+				39.736 0.000 72.992 0.000 12.773 0.000 153.668 82.705 39.690 8.080 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000
+			}
+			lastmonthincom1=1598.331
+			lastmonthincometabl1={
+				160.500 539.364 817.406 52.862 20.882 1.665 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 5.652
+			}
+			lastmonthexpens1=409.644
+			lastmonthexpensetabl1={
+				39.736 0.000 72.992 0.000 12.773 0.000 153.668 82.705 39.690 8.080 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000
+			}
+			totalexpensetabl1={
+				57281.152 8858.215 53707.281 151.200 35162.777 0.000 182451.328 41266.332 28286.047 10804.620 0.000 6979.546 15015.400 0.000 308928.188 0.000 65326.508 3621.690 16936.703 86527.047 0.000 0.000 0.000 0.000 0.000 0.000 0.000 6571.160 16624.076 157.956 0.000 36739.602 0.000 74.205 194.857 159112.000 27260.199 102.400
+			}
+			lastyearincom1={
+				1905.523 6395.835 9682.316 634.344 247.500 19.980 0.000 0.000 0.000 0.000 0.000 0.000 0.000 597.321 0.000 0.000 0.000 26.108 108.956
+			}
+			lastyearexpens1={
+				476.682 0.000 875.137 0.000 153.276 0.000 2102.549 992.460 426.870 80.840 0.000 0.000 575.700 0.000 21585.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 9108.000 0.000 0.000 0.000 0.000 0.000 0.000
+			}
+			last_months_recurring_incom1=1598.331
+			last_months_recurring_expense1=409.644
+"#;
+
+    let binary_data = include_bytes!("../../assets/ledger.bin");
+
     group.throughput(Throughput::Bytes(data.len() as u64));
 
-    group.bench_function("ledger", |b| {
+    group.bench_function("matching-ledger", |b| {
         b.iter(|| {
             let reader = jomini::text::TokenReader::from_slice(data.as_bytes());
+            let _: CountryLedger = jomini::TextDeserializer::from_windows1252_reader(reader)
+                .deserialize()
+                .unwrap();
+        })
+    });
+
+    let resolver = HashMap::<u16, String>::new();
+    group.bench_function("binary-ledger", |b| {
+        b.iter(|| {
+            let _: CountryLedger = jomini::BinaryDeserializer::builder_flavor(Eu4Flavor::new())
+                .deserialize_slice(&binary_data[..], &resolver)
+                .unwrap();
+        })
+    });
+
+    group.bench_function("skip-ledger", |b| {
+        b.iter(|| {
+            let reader = jomini::text::TokenReader::from_slice(data2.as_bytes());
             let _: CountryLedger = jomini::TextDeserializer::from_windows1252_reader(reader)
                 .deserialize()
                 .unwrap();
@@ -190,4 +273,8 @@ where
     }
 
     deserializer.deserialize_seq(ListVisitor)
+}
+
+fn array_default<const N: usize>() -> [f32; N] {
+    [0.0; N]
 }
