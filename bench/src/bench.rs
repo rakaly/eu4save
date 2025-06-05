67f8b7a1115aaae6baf6865684d98e12b7addf1f
Nick Babcock <nbabcock19@hotmail.com>
2025-06-05 18:15:38 -0500

    Add benchmarking of country tag hashing

--- bench/src/bench.rs
+++ bench/src/bench.rs
@@ -5,7 +5,11 @@ use serde::{
     de::{self, SeqAccess},
     Deserialize, Deserializer,
 };
-use std::{collections::HashMap, fmt};
+use std::{
+    collections::HashMap,
+    fmt,
+    hash::{BuildHasherDefault, Hasher},
+};
 
 fn parse_save(c: &mut Criterion) {
     let mut group = c.benchmark_group("parse_save");
@@ -227,7 +231,86 @@ fn deserialize_ledger(c: &mut Criterion) {
     group.finish();
 }
 
-criterion::criterion_group!(benches, parse_save, deserialize_ledger);
+fn country_tag_hashing(c: &mut Criterion) {
+    let mut group = c.benchmark_group("country_tag_hashing");
+
+    let data = std::fs::read("../assets/saves/mp_Uesugi.eu4").unwrap();
+    let file_data = std::fs::read("../assets/eu4.txt").unwrap_or_default();
+    let segments = eu4save::SegmentedResolver::parse(file_data.as_slice()).unwrap();
+    let file = eu4save::Eu4File::from_slice(&data).unwrap();
+    let save = file.parse_save(&segments.resolver()).unwrap();
+    let country_tags = save
+        .game
+        .countries
+        .iter()
+        .map(|(tag, _)| *tag)
+        .collect::<Vec<_>>();
+
+    group.throughput(Throughput::Elements(country_tags.len() as u64));
+    group.bench_function("default-hash", |b| {
+        let map = country_tags
+            .iter()
+            .enumerate()
+            .map(|(i, tag)| (*tag, i))
+            .collect::<HashMap<_, _>>();
+        b.iter(|| {
+            let mut sum = 0;
+            for tag in &country_tags {
+                sum += map.get(tag).unwrap_or(&0);
+            }
+            std::hint::black_box(sum)
+        })
+    });
+
+    type FnvBuildHasher = BuildHasherDefault<FnvHasher>;
+    pub type FnvHashMap<K, V> = HashMap<K, V, FnvBuildHasher>;
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct FnvHasher(u64);
+
+    impl Default for FnvHasher {
+        #[inline]
+        fn default() -> FnvHasher {
+            FnvHasher(0xcbf29ce484222325)
+        }
+    }
+
+    impl Hasher for FnvHasher {
+        #[inline]
+        fn finish(&self) -> u64 {
+            self.0
+        }
+
+        #[inline]
+        fn write(&mut self, bytes: &[u8]) {
+            let FnvHasher(mut hash) = *self;
+
+            for byte in bytes {
+                hash ^= *byte as u64;
+                hash = hash.wrapping_mul(0x100000001b3);
+            }
+
+            *self = FnvHasher(hash);
+        }
+    }
+
+    group.bench_function("fnv-hash", |b| {
+        let map = country_tags
+            .iter()
+            .enumerate()
+            .map(|(i, tag)| (*tag, i))
+            .collect::<FnvHashMap<_, _>>();
+        b.iter(|| {
+            let mut sum = 0;
+            for tag in &country_tags {
+                sum += map.get(tag).unwrap_or(&0);
+            }
+            std::hint::black_box(sum)
+        })
+    });
+}
+
+criterion::criterion_group!(benches, parse_save, deserialize_ledger, country_tag_hashing);
 criterion::criterion_main!(benches);
 
 fn collect_into_default<'de, A, T, const N: usize>(
